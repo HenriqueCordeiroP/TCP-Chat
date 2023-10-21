7b179addacac5a152d982f7c062ab3f71745b38e
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2023-10-21 00:45:03 -0300

    (feature): window

--- Client.py
+++ Client.py
@@ -1,7 +1,6 @@
 import socket
 import threading
-from utils import (PORT, IP, BUFFER_SIZE, compute_checksum, headers,
-                    get_message, get_checksum)
+from utils import PORT, IP, BUFFER_SIZE, compute_checksum, headers, get_message, get_checksum
 from datetime import datetime
 
 class Client:
@@ -10,9 +9,11 @@ class Client:
         self.nickname = input("Escolha seu apelido: ")
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((IP, PORT))
-         
+
         self.sequence_number = 0
         self.sequence_number_lock = threading.Lock()
+        self.window_size = 5
+        self.last_ack_received = 0
 
         receive_thread = threading.Thread(target=self.receive)
         receive_thread.start()
@@ -31,27 +32,32 @@ class Client:
                 data = self.client.recv(BUFFER_SIZE)
                 message = get_message(data)
                 if message == 'NICK': # saves the nickname
-                    self.client.send(headers({"message":self.nickname}))
+                    self.client.send(headers({"message": self.nickname}))
                 else:
                     print(f"{datetime.now().strftime('%H:%M')} {message}\n")
             except:
                 print("Ocorreu um erro!")
-                self.running = False
                 self.client.close()
                 break
 
     def write(self):
         # if message is 'sair', exits the server, else sends message to server
         while self.running:
-            message = '{}: {}'.format(self.nickname, input('> '))
-            if message == 'sair':
-                self.running = False
-                self.client_socket.close()
-            else:
-                self.increment_sequence_number()
-                data = {"sequence_number": self.sequence_number,"message":message}
-                json_data = headers(data)
-                self.client.send(json_data)
+            if self.sequence_number - self.last_ack_received < self.window_size:
+                message = '{}: {}'.format(self.nickname, input('> '))
+                if message.lower() == f'{self.nickname}: sair':
+                    self.running = False
+                    self.client.close()
+                else:
+                    self.increment_sequence_number()
+                    data = {"sequence_number": self.sequence_number, "message": message}
+                    json_data = headers(data)
+                    self.client.send(json_data)
+
+    def process_ack(self, ack_number):
+        self.last_ack_received = max(self.last_ack_received, ack_number)
 
 if __name__ == '__main__':
     client = Client()
+
+
--- Server.py
+++ Server.py
@@ -1,26 +1,26 @@
 import socket
 import threading
-from utils import (PORT, IP, BUFFER_SIZE, compute_checksum, headers,
-                    get_message, get_checksum, unpack_data)
+from utils import PORT, IP, BUFFER_SIZE, compute_checksum, headers, get_message, get_checksum, unpack_data
 import traceback
 import time
+
 class Server:
     nicknames = []
     clients = []
+
     def __init__(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((IP, PORT))
         self.server.listen()
-        
+
+        self.acknowledgements = {}
         self.message_timeout = 30
         self.nack_messages = {}
-
         self.sequence_number = 0
-
         self.sequence_number_lock = threading.Lock()
 
         self.receive()
-        
+
     def increment_sequence_number(self):
         with self.sequence_number_lock:
             self.sequence_number += 1
@@ -28,10 +28,8 @@ class Server:
     def broadcast(self, data):
         # sends message to every connected client
         data = unpack_data(data)
-        message = data['message']
-
-        data = {'message':message} # dictionary format for easier transfer
-        json_data = headers(data) # function that encodes the dictionary to json/ascii
+        message = data['message'] # dictionary format for easier transfer
+        json_data = headers({'message': message})   # function that encodes the dictionary to json/ascii
         for client in self.clients:
             client.send(json_data)
 
@@ -48,22 +46,20 @@ class Server:
                         break
                     else:
                         self.increment_sequence_number()
-
                         data = unpack_data(data)
                         data["sequence_number"] = self.sequence_number
                         data = headers(data)
-                        
-                        # add to nack dictonary. if message is sent, it will be removed
+
+                         # add to nack dictonary. if message is sent, it will be removed
                         self.nack_messages[message] = (client, data)
 
-                        # create and start timer thread 
+                         # create and start timer thread 
                         acknowledgment_timer = threading.Thread(target=self.timer, args=(client, data))
                         acknowledgment_timer.start()
-
                         self.broadcast(data)
                 else:
                     print("Soma de verificação inválida.")
-            except Exception as e:
+            except:
                 print(traceback.print_exc())
                 self.remove_client(client)
                 break
@@ -73,7 +69,6 @@ class Server:
         if not self.ack_ok(data):
             # handle timeout
             print("Timeout. Trying again...")
-
             self.broadcast(data)
 
     def ack_ok(self, data):
@@ -86,13 +81,10 @@ class Server:
     def receive(self):
         # connects client and broadcasts their connection
         while True:
-              
             client, address = self.server.accept()
             print("Conectado com {}".format(str(address)))
 
-            data = {"message":"NICK"}
-            json_data = headers(data)
-            client.send(json_data)
+            client.send(headers({"message": "NICK"}))
             data = client.recv(BUFFER_SIZE)
             received_checksum = get_checksum(data)
             nickname = get_message(data)
@@ -101,31 +93,22 @@ class Server:
             else:
                 self.nicknames.append(nickname)
                 self.clients.append(client)
-
-
                 print("{} conectou.".format(nickname))
-                
-                data = {"message":f"Bem vindo {nickname}! Digite \"sair\" para desconectar.\n"}
-                json_data = headers(data)
-                client.send(json_data)
-                
-                data = {"message":f"{nickname} entrou no chat!\n"}
-                json_data = headers(data)
-                self.broadcast(json_data)
-
-
+                client.send(headers({"message": f"Bem vindo {nickname}! Digite \"sair\" para desconectar.\n"}))
+                self.broadcast(headers({"message": f"{nickname} entrou no chat!\n"}))
                 thread = threading.Thread(target=self.handle, args=(client,))
                 thread.start()
 
     def remove_client(self, client):
         # disconnects client from server
-        client.send(headers({'message':"Até logo!"}))
+        client.send(headers({'message': "Até logo!"}))
         index = self.clients.index(client)
         self.clients.remove(client)
         client.close()
         nickname = self.nicknames[index]
-        self.broadcast(headers({"message":f"{nickname} saiu."}))
+        self.broadcast(headers({"message": f"{nickname} saiu."}))
         self.nicknames.remove(nickname)
 
 if __name__ == '__main__':
     server = Server()
+
